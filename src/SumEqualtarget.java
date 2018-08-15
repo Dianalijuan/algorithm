9ce59dedb779f0879f3b86913c57991dd7413c4b
Dianalijuan <2803089840@qq.com>
2018-08-15 21:42:01 +0800

    SumEqualtarget

--- src/SumEqualtarget.java
+++ src/SumEqualtarget.java
@@ -1,7 +1,9 @@
 import org.junit.Test;
+
 import java.util.*;
+
 /**
- *  给定一个整数数组和一个目标值，找出数组中和为目标值的两个数。
+ * 给定一个整数数组和一个目标值，找出数组中和为目标值的两个数。
  * 你可以假设每个输入只对应一种答案，且同样的元素不能被重复利用。
  */
 public class SumEqualtarget {
@@ -16,8 +18,6 @@ public class SumEqualtarget {
                 }
             }
         }
-
-
         return;
     }
 }
